1540030c2b7633e44b3bf98cd1a25f5f1d59fc73
Bossy325 <155837017+Bossy325@users.noreply.github.com>
2024-01-11 20:01:49 +0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    API_ID = int(os.environ.get("APP_ID", "26744258"))
+    API_HASH = os.environ.get("API_HASH", "7c7dbcb9e6654389d112b418e10583fa")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6468655747:AAFmFELeRiIKeYKfesaJ_kIIu1kMd0PA4sc")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOKMBu1IXDOJSEUBkuy0t4iiql5gEchsz-UXr_dEzSuZCSfU_mHyf_qeLJd_ezfc-NtrEKhT-l_MAkNDbcehkglpXJlFqs5f2p_50wXtaWO3mO3sdrV1ABuUFh-JRfKp4VpBQV9s8SN480OpkwYAHDB402VEynr9nwfrv8oDZWjI5U-89ECXq34OYfWDm7T7jNmkY6-oDf_b-dfgqfaTFL7F0nrDitbX7VYLwrA3FGYIYNo1wmXcsdJJJXlzff8fHinbe6KD7F6QKcmEFLrgg8IH8he9EYjNzdPR2aR_jh6RwIUs72L4ukwEWnxzY4_W-DGOx0AB54edPfzQstOl5DBqyHGQ=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Messi27_bot")
+    SUPPORT = os.environ.get("SUPPORT", "-1002015179601") # Your Support
+    CHANNEL = os.environ.get("CHANNEL", "-1002015179601") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3d8ecee0ba7dddfc6fce4.jpg")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "6266348511")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', "True") # Change it to "True"
